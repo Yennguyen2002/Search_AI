4ad09172f618e39fa86553157acf7d2cca94cb51
duykha0205 <63403660+duykha0205@users.noreply.github.com>
2022-06-29 14:02:17 +0700

    kha dfs

---
+++ Lab01/student_functions.py
@@ -0,0 +1,166 @@
+import numpy as np
+
+def kha_dfs(matrix, start, end, path, visited):
+    # Set current node as visited
+    path.append(start)
+    if start == end:
+        return
+
+    visited[start] = end
+
+    # For every node of the graph
+    for i in range(len(matrix)):
+        if (matrix[start][i] == 1 and ( i not in visited.items())):
+            DFS(matrix, i, end)
+
+def DFS(matrix, start, end):
+    """
+    BFS algorithm:
+    Parameters:
+    ---------------------------
+    matrix: np array 
+        The graph's adjacency matrix
+    start: integer
+        starting node
+    end: integer
+        ending node
+    
+    Returns
+    ---------------------
+    visited
+        The dictionary contains visited nodes, each key is a visited node,
+        each value is the adjacent node visited before it.
+    path: list
+        Founded path
+    """
+    # TODO: 
+   
+    path=[]
+    visited={}
+    #kha_dfs(matrix, start, end, path, visited)
+
+    stack = [start]
+
+    while len(stack) != 0:
+        print("----", stack)
+        vertex = stack.pop()
+        path.append(vertex)
+        if vertex == end:
+            break
+        # if vertex in path:
+        #     continue
+        visited[vertex] = end
+        for i in range(len(matrix)):
+            if (matrix[vertex][i] == 1 and ( i not in visited.items())):
+                stack.append(i)
+
+    print(path)
+
+    return visited, path
+
+def BFS(matrix, start, end):
+    """
+    DFS algorithm
+     Parameters:
+    ---------------------------
+    matrix: np array 
+        The graph's adjacency matrix
+    start: integer 
+        starting node
+    end: integer
+        ending node
+    
+    Returns
+    ---------------------
+    visited 
+        The dictionary contains visited nodes: each key is a visited node, 
+        each value is the key's adjacent node which is visited before key.
+    path: list
+        Founded path
+    """
+
+    # TODO: 
+    
+    path=[]
+    visited={}
+   
+    return visited, path
+
+
+def UCS(matrix, start, end):
+    """
+    Uniform Cost Search algorithm
+     Parameters:visited
+    ---------------------------
+    matrix: np array 
+        The graph's adjacency matrix
+    start: integer 
+        starting node
+    end: integer
+        ending node
+    
+    Returns
+    ---------------------
+    visited
+        The dictionary contains visited nodes: each key is a visited node, 
+        each value is the key's adjacent node which is visited before key.
+    path: list
+        Founded path
+    """
+    # TODO:  
+    path=[]
+    visited={}
+    return visited, path
+
+
+def GBFS(matrix, start, end):
+    """
+    Greedy Best First Search algorithm
+     Parameters:
+    ---------------------------
+    matrix: np array 
+        The graph's adjacency matrix
+    start: integer 
+        starting node
+    end: integer
+        ending node
+   
+    Returns
+    ---------------------
+    visited
+        The dictionary contains visited nodes: each key is a visited node, 
+        each value is the key's adjacent node which is visited before key.
+    path: list
+        Founded path
+    """
+    # TODO: 
+    path=[]
+    visited={}
+    return visited, path
+
+def Astar(matrix, start, end, pos):
+    """
+    A* Search algorithm
+     Parameters:
+    ---------------------------
+    matrix: np array UCS
+        The graph's adjacency matrix
+    start: integer 
+        starting node
+    end: integer
+        ending node
+    pos: dictionary. keys are nodes, values are positions
+        positions of graph nodes
+    Returns
+    ---------------------
+    visited
+        The dictionary contains visited nodes: each key is a visited node, 
+        each value is the key's adjacent node which is visited before key.
+    path: list
+        Founded path
+    """
+    # TODO: 
+    path=[]
+    visited={}
+    return visited, path
+
---
+++ Lab01/test.py
@@ -0,0 +1,9 @@
+a = {}
+a[1]  = 1
+a[2] = None
+print(a)
+print(type(a[1]))
+print(2 not in a.items())
+
+b = [1,2,3,4]
+print(b[-2])
